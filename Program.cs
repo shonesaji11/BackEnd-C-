c574ec86e42e64556ca0e7410f317b7b0c225748
Shone Saji <66723087+shonesaji11@users.noreply.github.com>
2021-09-09 14:28:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            switch (x)
+            {
+                case 5:
+                    Console.WriteLine("Value of x is 5");
+                    break;
+                case 10:
+                    Console.WriteLine("Value of x is 10");
+                    break;
+                case 15:
+                    Console.WriteLine("Value of x is 15");
+                    break;
+                default:
+                    Console.WriteLine("Unknown value");
+                    break;
+            }
+        }
+
+        static bool isGreater(int i, int j)
+        {
+            if (i < j)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
