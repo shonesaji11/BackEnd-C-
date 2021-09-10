bbf3b6036cfc38f977ca681ef031a13df4d80dc1
Shone Saji <66723087+shonesaji11@users.noreply.github.com>
2021-09-10 10:21:51 +0300

    Add files via upload

---
+++ Day2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ArrayDemo
+    {
+        public void Trial()
+
+        {
+            int[] array1 = { 1, 2, 4, 6, 8, 9, 11, 14, 15 };
+            var a = from i in array1 where i % 2 == 0 select i;
+            Console.WriteLine("\n\nInside class1.cs ");
+            foreach (var _a in a)
+            {
+                Console.WriteLine(_a);
+            }
+            Console.WriteLine("\n\n ");
+            //Console.ReadLine();
+        }
+
+        public void addNumber()
+        {
+            int a;
+            int b;
+            Console.WriteLine("Enter some input : ");
+        }
+    }
+}
