497ffbea46dcacd7090d779be7695906bd1543b1
Shone Saji <66723087+shonesaji11@users.noreply.github.com>
2021-09-16 13:40:50 +0300

    Add files via upload

---
+++ Day5/DAY5-Delegate/Class3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DAY5_Delegate
+{
+    class Class3
+    {
+        public void class3fn()
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 10;
+
+            System.Collections.ArrayList al = new System.Collections.ArrayList();
+            Console.WriteLine("\n\n\nArray and ArrayList \nSize of ArrayList a1 is , {0}" , al.Capacity);
+
+        }
+
+        public void printInfo(List<dynamic> list)
+        {
+            foreach(dynamic no in list)
+            {
+                Console.Write(no);
+                Console.Write("\t");
+            }
+        }
+
+        public void sortDemo()
+        {
+            //We use List<int> for two reasons
+            //1. we get the type safe property of simply int[]
+            //2.we get the dynamic capability of ArrayList ; ArrayList has the drawback of not being type safe
+
+            List<dynamic> list = new List<dynamic>() { 8, 6, 4, 5, 3, 2, 7, 9 };
+            Console.WriteLine("\n\n\n Sorting Demo");
+            Console.WriteLine("Before Sorting");
+            printInfo(list);
+            list.Sort();
+            Console.WriteLine("\nAfter Sorting");
+            printInfo(list);
+
+
+            //String List
+
+            List<dynamic> str_list = new List<dynamic>() { "Z", "B", "A", "D", "C" };
+            Console.WriteLine("\n\nBefore Sorting");
+            printInfo(str_list);
+            str_list.Sort();
+            Console.WriteLine("\nAfter Sorting");
+            printInfo(str_list);
+
+        }
+    }
+}
--- Day5/DAY5-Delegate/Program.cs
+++ Day5/DAY5-Delegate/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace DAY5_Delegate
 {
@@ -14,7 +15,7 @@ namespace DAY5_Delegate
 
     public delegate bool promotedDelegate(Employee empl);
     
-    public class Employee
+    public class Employee: IComparable<Employee>
         {
             public int Id { get; set; }
             public string Name { get; set; }
@@ -32,22 +33,55 @@ namespace DAY5_Delegate
                 }
             }
 
-            /*public void PromotedS(List<Employee> empList , promotedDelegate prom)
+            public static void DisplayEmployeeInfo(List<Employee> empList)
+                {
+                    foreach (Employee employee in empList)
+                    {
+                            Console.WriteLine("Employee Name : {0}, Employee Salary : {1}" , employee.Name , employee.Salary);
+                    }
+                }
+
+            public int CompareTo(Employee other)
             {
-                
-            }*/
+            if (this.Id > other.Id)
+            {
+                return 1;
+            } else if (this.Id < other.Id)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+            }
 
+
+
+        /*public void PromotedS(List<Employee> empList , promotedDelegate prom)
+        {
+
+        }*/
+    }
+
+
+    public class SortEmployeeById : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            return x.Salary.CompareTo(y.Salary);
         }
+    }
 
 
-        class Program
+    class Program
         {
             static void Main()
             {
                 Employee obj = new Employee();
                 List<Employee> emp = new List<Employee>();
 
-                emp.Add(new Employee() { Id = 101, Name = "Mark", Salary = 5000, Experience = 8 });
+                emp.Add(new Employee() { Id = 106, Name = "Mark", Salary = 5000, Experience = 8 });
                 emp.Add(new Employee() { Id = 102, Name = "Smith", Salary = 3000, Experience = 6 });
                 emp.Add(new Employee() { Id = 103, Name = "Watson", Salary = 6000, Experience = 3 });
                 emp.Add(new Employee() { Id = 104, Name = "Paul", Salary = 8000, Experience = 5 });
@@ -55,12 +89,50 @@ namespace DAY5_Delegate
 
                 promotedDelegate promSal = new promotedDelegate(isPromotedSal);
                 promotedDelegate promExp = new promotedDelegate(isPromotedExp);
+
+                Console.WriteLine("Displaying employee information");
+            //sorting a custom data type - here employee
+
+            //first way of sorting
+            //emp.Sort();
+
+
+            //second way of sorting
+            SortEmployeeById sortEmployeebyIdobj = new SortEmployeeById();
+            emp.Sort(sortEmployeebyIdobj);
+            Employee.DisplayEmployeeInfo(emp);
+
+            Console.WriteLine("\n\n\n");
+
+            //third way of sorting
+            //Comparison<Employee> comparison = ComparisonById;
+            //emp.Sort(comparison);
+
+            //another way
+            /*Comparison<Employee> comparison = delegate (Employee e1, Employee e2)
+            {
+                return e1.Id.CompareTo(e2.Id);
+            };*/
+
+            Comparison<Employee> comparison = (e1, e2) =>
+            {
+                return e1.Id.CompareTo(e2.Id);
+            };
+            emp.Sort(comparison);
+            Employee.DisplayEmployeeInfo(emp);
+
+
+
+
+
+
                 
-                Console.WriteLine("Promotion Based on Salary - ");
+                Console.WriteLine("\n\n\nPromotion Based on Salary - ");
                 Employee.Promoted(emp, promSal);
                 Console.WriteLine("Promotion Based on Experience - ");
                 Employee.Promoted(emp, promExp);
             //obj.PromotedS(emp, promExp);
+
 
             //Demonstrating various ways to use delegate
             Class1 classobj = new Class1();
@@ -71,6 +143,14 @@ namespace DAY5_Delegate
             //standard delegates - func(when you have a return type) ; action(when you do not have a return type) ; predicate(when you have bool return type)
             Class2 classobj2 = new Class2();
             classobj2.class2fn();
+
+
+            //Defining array and arrayList
+            Class3 classobj3 = new Class3();
+            classobj3.class3fn();
+
+            //Sorting Demo
+            classobj3.sortDemo();
 
             Console.ReadKey();
 
@@ -99,6 +179,11 @@ namespace DAY5_Delegate
                     return false;
                 }
             }
+
+            public static int ComparisonById(Employee e1, Employee e2)
+        {
+            return e1.Id.CompareTo(e2.Id);
+        }
     }
 
 
